8f852fe73bfd7761d0682946337496ba36e5ff2a
ValliappanValliappan <gtvalliappan@gmail.com>
2017-07-08 14:14:54 -0700

    Created a class for Bluetooth sharing and removed an unnecessary menu

---
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/BluetoothSharing.java
@@ -0,0 +1,11 @@
+package com.competitionapp.nrgscouting;
+
+/**
+ * Created by valli on 7/8/17
+ */
+
+public class BluetoothSharing {
+    //Method 1:Connect
+
+    //Method 2:Share data
+}
--- NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MainActivity.java
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MainActivity.java
@@ -67,9 +67,6 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
