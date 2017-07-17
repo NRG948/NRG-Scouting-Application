3ede7b6349d0daf37bba09fc36b36c7a8c2fb546
Acchindra Thev <acchinthev@gmail.com>
2017-07-16 20:22:35 -0700

    Finished Basic Layout for "About Tab"

--- NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/About.java
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/About.java
@@ -23,6 +23,7 @@ public class About extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 
