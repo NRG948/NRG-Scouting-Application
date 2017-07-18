b16abd0387cb3047250c78b122ed7ee0dea17486
Nipun Chhajed <nipunchhajed@gmail.com>
2017-07-17 17:44:12 -0700

    made changes to make our app have more functionality suited to our app's needs.

--- NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MainActivity.java
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add Teams", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
