2d5323069f6a88b7a8af33675e718a0e2168a055
ValliappanValliappan <gtvalliappan@gmail.com>
2017-07-19 14:34:40 -0700

    Setting up for ranking algorithm
    I’m soooooooooooooooooo awesome

---
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/Ranker.java
@@ -0,0 +1,22 @@
+package com.competitionapp.nrgscouting;
+
+/**
+ * Created by valli on 7/19/17.
+ */
+
+public class Ranker {
+    //Weightages for each aspect of the comparison to see how much percent are they a good match for us
+    //Higher percent better match. This percent is the rankscore of Team objects
+    //Autonomous and telop scores are calculated using weightages and rank score is calculated by assigning a weightage to both of thos scores
+    Ranker(Team[] teamsToRank){
+        //Loop through all teams
+            //Find autonomous score and teleoperated score
+            //Add scores
+            //Store in rankScore variable of team
+
+
+        //New loop through all teams
+            //Reorder teamsToRank array in the order of lest to greatest rankScore
+
+    }
+}
---
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/Team.java
@@ -0,0 +1,22 @@
+package com.competitionapp.nrgscouting;
+
+/**
+ * Created by valli on 7/19/17.
+ */
+
+public class Team {
+    int maxTotalPointsPossiblePerMatch;
+    String name;
+    int expectedTotalBallsScored;
+    int totalBallsScored;
+    int totalMatchesPlayed;
+    double position;
+    double totalGearsOnHook;
+    double totalDeaths;
+    double rankScore;
+    double totalRedOrYellowCards;
+    int noConflictBetweenTeams;//1 or 0(True or false)
+    int shooterRobot;//1 or 0(True or false)
+    int gearScoringRobot;//1 or 0(True or false)
+
+}
