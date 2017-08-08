5458eaa09169a613b66dd48174218fd8c7b09a1f
ValliappanValliappan <gtvalliappan@gmail.com>
2017-08-07 18:24:23 -0700

    Cool. If the exists, the list works...... according to me

--- NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MatchActivity.java
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MatchActivity.java
@@ -1,38 +1,34 @@
 package com.competitionapp.nrgscouting;
 import java.io.File;
-import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class MatchActivity extends MainActivity {
     ListView listView;
     ArrayAdapter<String> teamAdapter;
-    String[] matchTeams = {"948", "492",};
+    String[] matchTeams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        teamAdapter = new ArrayAdapter<String>(MatchActivity.this, android.R.layout.simple_list_item_1, matchTeams);
         setContentView(R.layout.activity_match);
         if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
             File externalStoreDir = Environment.getExternalStorageDirectory();
             File entries = new File(externalStoreDir,"Entries.txt");
             ArrayList<Entry> list=MatchEntry.getAllEntriesInFileIntoObjectForm(entries);
-            for(Entry a:list){
-                teamAdapter.add("Match:"+a.matchNumber+"   Team:"+a.teamName);
+            matchTeams=new String[list.size()];
+            teamAdapter = new ArrayAdapter<String>(MatchActivity.this, android.R.layout.simple_list_item_1, matchTeams);
+            for(int i=0;i<=matchTeams.length;i++){
+                matchTeams[i]="Match:"+list.get(i).matchNumber+"   Team:"+list.get(i).teamName;
             }
+        }
+        else{
+            System.out.println("File not found...");
         }
         listView = (ListView)findViewById(R.id.list_team);
         listView.setAdapter(teamAdapter);
