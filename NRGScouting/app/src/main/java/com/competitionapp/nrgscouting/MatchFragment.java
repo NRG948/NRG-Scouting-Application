a0b645f4bd53fd55792f2d21b423c6406057b76e
ValliappanValliappan <gtvalliappan@gmail.com>
2017-08-07 18:55:09 -0700

    Some small fixes bcoz of Android studio

--- NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MatchFragment.java
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MatchFragment.java
@@ -14,15 +14,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import java.io.File;
+import android.os.Environment;
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MatchFragment extends Fragment {
-    ListView lv;
-    ArrayAdapter<String> adapter1;
-    String[] entries = {"NRG entry", "Titans entry", "Totem Robotics entry"};
+    ListView listView;
+    ArrayAdapter<String> teamAdapter;
+    String[] matchTeams;
 
     public MatchFragment() {
         // Required empty public constructor
@@ -35,17 +39,16 @@ public class MatchFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_match, container, false);
 
         //List and Search view initializations
-        lv= (ListView)rootView.findViewById(R.id.teams);
+        listView= (ListView)rootView.findViewById(R.id.teams);
 
         //ListView set up
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_match);
         if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
             File externalStoreDir = Environment.getExternalStorageDirectory();
             File entries = new File(externalStoreDir,"Entries.txt");
             ArrayList<Entry> list=MatchEntry.getAllEntriesInFileIntoObjectForm(entries);
             matchTeams=new String[list.size()];
-            teamAdapter = new ArrayAdapter<String>(MatchActivity.this, android.R.layout.simple_list_item_1, matchTeams);
+            teamAdapter = new ArrayAdapter<String>(MatchFragment.this, android.R.layout.simple_list_item_1, matchTeams);
             for(int i=0;i<=matchTeams.length;i++){
                 matchTeams[i]="Match:"+list.get(i).matchNumber+"   Team:"+list.get(i).teamName;
             }
