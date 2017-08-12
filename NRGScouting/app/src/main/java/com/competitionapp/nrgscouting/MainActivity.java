8daab3b1772f2d53d96742f2546fca34fe37342c
Adam-Nicewarner <nicewarneradam@gmail.com>
2017-08-12 11:37:04 -0700

    Specialist Fragment
    stuff about the specialist fragment

--- NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MainActivity.java
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MainActivity.java
@@ -49,6 +49,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        
     }
 
     @Override
