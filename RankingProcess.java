e6f146a39c7ec14a6e5fa644d769f8215c8f86b5
ValliappanValliappan <gtvalliappan@gmail.com>
2017-09-16 19:16:59 -0700

    The algorithm is not working
    Need to fix a lot

--- RankingProcess.java
+++ RankingProcess.java
@@ -3,32 +3,34 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class RankingProcess {
-
-	public static void main(String[] args) {
-		/**
-		 * The path for the folder that is storing all Entries text files goes in the next line of code
-		 */
-		File folder = new File("/Users/valli/Desktop/WorldOfEntries");
-		File[] list = folder.listFiles();//List of files with Entries
-		EntriesToTeamObjects.listOfEntriesInFile = new ArrayList<Entry>();
-		for (File i : list) {
-			try{//For the sake of satisfying compiler
-				EntriesToTeamObjects.getAllEntriesInFileIntoObjectForm(i, EntriesToTeamObjects.readFile(i));
-			}
-			catch(FileNotFoundException e){
-				//Do nothing
-			}
-		}
-		Ranker ranker=new Ranker(EntriesToTeamObjects.teams);
-		/**
-		 * THE SORTED LIST IS THE ONE THAT IS PRINTED
-		 */
-		for(Team team:ranker.sortedListOfTeams){
-			System.out.println("Score: "+team.rankScore+" Name:"+team.name);
-		}
-		/**
-		 * 
-		 */
-	}
-
+    
+    public static void main(String[] args) {
+        /**
+         * The path for the folder that is storing all Entries text files goes
+         * in the next line of code
+         */
+        File folder = new File("/Users/valli/Desktop/WorldOfEntries");
+        File[] list = folder.listFiles();// List of files with Entries
+        list[0] = new File("");//IF ALGORITHM IS RUN ON A MAC
+        EntriesToTeamObjects.listOfEntriesInFile = new ArrayList<Entry>();
+        for (File i : list) {
+            try {// For the sake of satisfying compiler
+                EntriesToTeamObjects.getAllEntriesInFileIntoObjectForm(i, EntriesToTeamObjects.readFile(i));
+            } catch (FileNotFoundException e) {
+                // Do nothing
+            }
+        }
+        Ranker ranker = new Ranker(EntriesToTeamObjects.teams);
+        /**
+         * THE SORTED LIST IS THE ONE THAT IS PRINTED
+         */
+        for (Team team : ranker.sortedListOfTeams) {
+            System.out.println("Score: " + team.rankScore + " Name:" + team.name);
+        }
+        /**
+         * 
+         */
+    }
+    
 }
+
