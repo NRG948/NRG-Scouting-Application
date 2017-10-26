0ca987a047dbe89d97ef6eac80a02579db111141
Nipun Chhajed <nipunchhajed@gmail.com>
2017-10-26 09:37:31 -0700

    tanks

--- NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/RefreshableFragment.java
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/RefreshableFragment.java
@@ -1,7 +1,7 @@
 package com.competitionapp.nrgscouting;
 
 /**
- * Created by Peyton Lee on 9/19/2017. 
+ * Created by Peyton Lee on 9/19/2017. Approved By Acchindra. 
  */
 
 public interface RefreshableFragment {
