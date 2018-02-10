66bc9cd97de114f896a26e64f89c2ff98b222578
Peyton L <WolfboxW@users.noreply.github.com>
2018-02-10 01:06:08 -0800

    Implemented Tabbed Activities
    (Currently the Tabbed Activities are the default activity, which I can reset if need be-- this will be a little easier for dev purposes)

---
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/TabbedActivity.java
@@ -0,0 +1,97 @@
+package com.competitionapp.nrgscouting;
+
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.View;
+
+/**
+ * Created by Peyton Lee on 2/9/2018.
+ */
+
+public class TabbedActivity extends AppCompatActivity implements ActivityUtility{
+
+    ViewPager viewPager;
+    MatchTabFragmentAdapter tabAdapter;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tabbed_view_layout);
+
+        setActionBarTitle("Match Entry");
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_timer));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_picked_cube));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_drop_cube));
+
+
+
+        tabAdapter = new MatchTabFragmentAdapter(getSupportFragmentManager());
+        viewPager = (ViewPager) findViewById(R.id.tabViewPager);
+        viewPager.setAdapter(tabAdapter);
+
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    public void setActionBarTitle(String title) {
+        getSupportActionBar().setTitle(title);
+    }
+
+    public static class MatchTabFragmentAdapter extends FragmentPagerAdapter {
+
+        public MatchTabFragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    MatchTimerEntry matchTimerEntry0 = new MatchTimerEntry();
+                    return matchTimerEntry0;
+                case 1:
+                    MatchTimerEntry matchTimerEntry1 = new MatchTimerEntry();
+                    return matchTimerEntry1;
+                case 2:
+                    MatchEntry matchTimerEntry2 = new MatchEntry();
+                    return matchTimerEntry2;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+
+}
+
+
