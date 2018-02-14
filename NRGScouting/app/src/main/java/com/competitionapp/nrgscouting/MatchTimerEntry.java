ef5b5c7bbc4d760d55f172b0820e34e01312b1b9
Nipun Chhajed <nipunchhajed@gmail.com>
2018-02-13 22:04:09 -0800

    my android studio did something weird

--- NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MatchTimerEntry.java
+++ NRGScouting/app/src/main/java/com/competitionapp/nrgscouting/MatchTimerEntry.java
@@ -10,22 +10,13 @@ import android.widget.Button;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Peyton Lee on 2/9/2018.
  */
 
 public class MatchTimerEntry extends Fragment {
-    private Button eatPowerCube;
-    private Button allySwitch;
-    private Button oppSwitch;
-    private Button scale;
-    private Button boost;
-    private Button cubeDrop;
-    private Button forceUsed;
-    private Button climbStart;
-    private Button timer;
-    private Thread thread;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,34 +27,6 @@ public class MatchTimerEntry extends Fragment {
     }
     @Override
     public void onStart() {
-        eatPowerCube = (Button)(getView().findViewById(R.id.eat_powercube));
-        allySwitch = (Button)(getView().findViewById(R.id.ally_switch));
-        oppSwitch = (Button)(getView().findViewById(R.id.opp_switch));
-        scale = (Button)(getView().findViewById(R.id.claimed_scale));
-        boost = (Button)(getView().findViewById(R.id.boost_used));
-        cubeDrop = (Button)(getView().findViewById(R.id.cube_dropped));
-        forceUsed = (Button)(getView().findViewById(R.id.force_used));
-        climbStart = (Button)(getView().findViewById(R.id.climb_start));
-        timer = (Button)(getView().findViewById(R.id.time_start));
-        timer.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                thread = new Thread() {
-                    @Override
-                    public void run() {
-                        while(!isInterrupted()) {
-                            try {
-                                thread.sleep(150000);
-                                if(isInterrupted()) {
-                                    
-                                }
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                };
-            }
-        });
         super.onStart();
     }
 }
